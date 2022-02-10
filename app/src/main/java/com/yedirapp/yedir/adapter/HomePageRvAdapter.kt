78b2138786c4a7e9b5d90ea6240b26ef9e5c3d48
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-02-10 21:16:38 +0300

    HomePageRvAdapter.kt abd home_page_card_design.xml add project

---
+++ app/src/main/java/com/yedirapp/yedir/adapter/HomePageRvAdapter.kt
@@ -0,0 +1,47 @@
+package com.yedirapp.yedir.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.yedirapp.yedir.databinding.HomePageCardDesignBinding
+import com.yedirapp.yedir.entity.Foods
+import com.yedirapp.yedir.viewmodel.HomePageViewModel
+
+class HomePageRvAdapter(var mContext: Context, var foodsList: List<Foods>, var viewModel: HomePageViewModel) :
+RecyclerView.Adapter<HomePageRvAdapter.CardDesignHolder>() {
+
+    inner class CardDesignHolder(cardDesignBinding: HomePageCardDesignBinding) :
+        RecyclerView.ViewHolder(cardDesignBinding.root) {
+        var cardDesignBinding: HomePageCardDesignBinding
+
+        init {
+            this.cardDesignBinding = cardDesignBinding
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardDesignHolder {
+        val layoutInflater = LayoutInflater.from(mContext)
+        val tasarim = HomePageCardDesignBinding.inflate(layoutInflater, parent, false)
+        return CardDesignHolder(tasarim)
+    }
+
+    override fun onBindViewHolder(holder: CardDesignHolder, position: Int) {
+        val food = foodsList.get(position)
+        val t = holder.cardDesignBinding
+        t.foodObj = food
+
+       /* t.satirCard.setOnClickListener {
+            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi)
+            Navigation.findNavController(it).navigate(gecis)
+        }*/
+
+    }
+
+    override fun getItemCount(): Int {
+        return foodsList.size
+    }
+
+}
--- app/src/main/java/com/yedirapp/yedir/fragment/HomePageFragment.kt
+++ app/src/main/java/com/yedirapp/yedir/fragment/HomePageFragment.kt
@@ -7,22 +7,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.yedirapp.yedir.R
+import com.yedirapp.yedir.adapter.HomePageRvAdapter
 import com.yedirapp.yedir.databinding.FragmentHomePageBinding
-import com.yedirapp.yedir.databinding.FragmentViewPagerBinding
+import com.yedirapp.yedir.viewmodel.HomePageViewModel
 
 class HomePageFragment : Fragment() {
     private lateinit var binding: FragmentHomePageBinding
-    private lateinit var bindingViewPager: FragmentViewPagerBinding
-
+    private lateinit var adapter: HomePageRvAdapter
+    private lateinit var viewModel: HomePageViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val tempViewModel: HomePageViewModel by viewModels()
+        viewModel = tempViewModel
     }
 
     fun onClick() {
         Log.e("Deneme2", "Denemeee")
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +35,13 @@ class HomePageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_page, container, false)
-        binding.homePageObj=this
+        binding.homePageObj = this
+        viewModel.foodsList.observe(viewLifecycleOwner, {
+            adapter = HomePageRvAdapter(requireContext(), it, viewModel)
+            binding.homePageRvAdapter = adapter
+            binding.homePageRv.layoutManager =
+                StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL)
+        })
         return binding.root
     }
 
