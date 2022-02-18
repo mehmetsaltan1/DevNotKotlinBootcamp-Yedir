2c097ffac3fcde059c6a4e8d534a1c92cfb2cff3
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-02-18 20:45:26 +0300

    Custom alert dialog add project

--- app/src/main/java/com/yedirapp/yedir/fragment/HomePageFragment.kt
+++ app/src/main/java/com/yedirapp/yedir/fragment/HomePageFragment.kt
@@ -1,16 +1,19 @@
 package com.yedirapp.yedir.fragment
 
 import android.os.Bundle
-import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.squareup.picasso.Picasso
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.yedirapp.yedir.R
 import com.yedirapp.yedir.adapter.HomePageRvAdapter
 import com.yedirapp.yedir.databinding.FragmentHomePageBinding
@@ -21,6 +24,7 @@ class HomePageFragment : Fragment() {
     private lateinit var binding: FragmentHomePageBinding
     private lateinit var adapter: HomePageRvAdapter
     private lateinit var viewModel: HomePageViewModel
+    private var food_total: Int = 1
     val userName = "mehmet_saltan"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +37,28 @@ class HomePageFragment : Fragment() {
     }
 
     fun onClickAddFood(
-        food_name: String,
-        food_image_name: String,
-        food_price: Int,
-        food_total: Int,
+        foodObj: Foods,
         username: String
     ) {
-        viewModel.addFoodBasket(food_name, food_image_name, food_price, food_total, username)
+        val designAlertlayout = layoutInflater.inflate(R.layout.custom_alert_dialog_design, null)
+        val editTextAlert = designAlertlayout.findViewById(R.id.customInputText) as EditText
+        MaterialAlertDialogBuilder(requireActivity(), R.style.MaterialAlertDialog_rounded)
+            .setView(designAlertlayout)
+            .setPositiveButton("Ekle") { dialog, which ->
+                food_total = editTextAlert.text.toString().toInt()
+                viewModel.addFoodBasket(
+                    foodObj.food_name,
+                    foodObj.food_image_name,
+                    foodObj.food_price,
+                    food_total,
+                    username
+                )
+                Toast.makeText(this.requireContext(),"${"$food_total Adet ${foodObj.food_name} sepete eklendi"}",Toast.LENGTH_LONG).show()
+              //  myToast.show()
+            }
+            .setNegativeButton("İptal", null)
+            .create()
+            .show()
     }
 
 
@@ -50,9 +69,10 @@ class HomePageFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_page, container, false)
         binding.homePageObj = this
+
         viewModel.foodsList.observe(viewLifecycleOwner, {
             var foodList = it
-            viewModel.foodsDescriptionList.observe(viewLifecycleOwner){
+            viewModel.foodsDescriptionList.observe(viewLifecycleOwner) {
                 adapter = HomePageRvAdapter(
                     requireContext(), foodList,
                     it, this
